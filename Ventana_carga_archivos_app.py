eb0f5b4ffbf8b803af25eb5d1470ae3275958a27
foreman7684 <foreman7684@gmail.com>
2024-04-11 00:35:14 -0300

    Update Ventana_carga_archivos_app.py

--- Ventana_carga_archivos_app.py
+++ Ventana_carga_archivos_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+from base64 import b64encode
 
 # Título de la aplicación
 st.title('Subida de Archivos a Snowflake')
@@ -13,7 +14,7 @@ uploaded_file = st.file_uploader("Elige un archivo")
 if uploaded_file is not None:
     file_name = uploaded_file.name
     file_type = uploaded_file.type
-    file_value = uploaded_file.getvalue()
+    file_value = b64encode(uploaded_file.getvalue()).decode('utf-8')
     my_insert_stmt = """ insert into POC_RAUL.public.REPOSITORIO_ARCHIVOS_INTERNET(NOMBRE_ARCHIVO,EXTENSION,ARCHIVO)
     values ('""" + file_name + """','""" +file_type+"""','""" +file_value+"""')"""
     session.sql(my_insert_stmt).collect()
