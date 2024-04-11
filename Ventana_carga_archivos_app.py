345ad92d7eeb5ab17a5d6f8eaed87bb4fb218054
foreman7684 <foreman7684@gmail.com>
2024-04-11 00:10:43 -0300

    Update Ventana_carga_archivos_app.py

--- Ventana_carga_archivos_app.py
+++ Ventana_carga_archivos_app.py
@@ -11,9 +11,8 @@ session = cnx.session()
 uploaded_file = st.file_uploader("Elige un archivo")
 
 if uploaded_file is not None:
-    file_name = {
-        "FileName":uploaded_file.name
-    }
+    file_name = uploaded_file.name
+
     my_insert_stmt = """ insert into POC_RAUL.public.REPOSITORIO_ARCHIVOS_INTERNET(NOMBRE_ARCHIVO)
     values ('"""file_name"""')"""
     # Aquí puedes procesar tu archivo como necesites
