2de987b0d12d50ba766a79419fd86b178596e7d8
foreman7684 <foreman7684@gmail.com>
2024-04-10 23:58:49 -0300

    Update Ventana_carga_archivos_app.py

--- Ventana_carga_archivos_app.py
+++ Ventana_carga_archivos_app.py
@@ -1,19 +1,21 @@
 import streamlit as st
+from snowflake.snowpark.functions import col
 
 # Título de la aplicación
 st.title('Subida de Archivos a Snowflake')
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Crear un selector de archivos
 uploaded_file = st.file_uploader("Elige un archivo")
 
 if uploaded_file is not None:
+    file_name = {
+    "FileName":uploaded_file.name
+    }
+    my_insert_stmt = """ insert into POC_RAUL.public.REPOSITORIO_ARCHIVOS_INTERNET(NOMBRE_ARCHIVO)
+    values ('""" +FileName+"""')"""
     # Aquí puedes procesar tu archivo como necesites
     # Por ejemplo, mostrar un mensaje de confirmación
     st.success('Archivo cargado con éxito!')
-
-    # Opcional: Muestra información sobre el archivo cargado
-    file_details = {
-        "FileName": uploaded_file.name,
-        "FileType": uploaded_file.type,
-        "FileSize": uploaded_file.size
-    }
