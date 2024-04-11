270d9ce4cf750dab700e2124c1edb23f055b1141
foreman7684 <foreman7684@gmail.com>
2024-04-11 00:20:54 -0300

    Update Ventana_carga_archivos_app.py

--- Ventana_carga_archivos_app.py
+++ Ventana_carga_archivos_app.py
@@ -12,9 +12,10 @@ uploaded_file = st.file_uploader("Elige un archivo")
 
 if uploaded_file is not None:
     file_name = uploaded_file.name
+    file_type = uploaded_file.type
 
-    my_insert_stmt = """ insert into POC_RAUL.public.REPOSITORIO_ARCHIVOS_INTERNET(NOMBRE_ARCHIVO)
-    values ('""" + file_name + """')"""
+    my_insert_stmt = """ insert into POC_RAUL.public.REPOSITORIO_ARCHIVOS_INTERNET(NOMBRE_ARCHIVO,EXTENSION)
+    values ('""" + file_name + """','""" +file_type+"""')"""
     session.sql(my_insert_stmt).collect()
     # Aquí puedes procesar tu archivo como necesites
     # Por ejemplo, mostrar un mensaje de confirmación
